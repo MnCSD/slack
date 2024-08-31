e67249d71b53f52327a16d718a9c132df02c44ba
MnCSD <mnikolopoylos@gmail.com>
2024-08-31 14:13:52 +0300

    join code and join page

---
+++ src/features/workspaces/api/use-get-workspace-info.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+interface UserGetWorkspaceInfoProps {
+  id: Id<"workspaces">;
+}
+
+export const useGetWorkspaceInfo = ({ id }: UserGetWorkspaceInfoProps) => {
+  const data = useQuery(api.workspaces.getInfoById, { id });
+  const isLoading = data === undefined;
+
+  return {
+    data,
+    isLoading,
+  };
+};
