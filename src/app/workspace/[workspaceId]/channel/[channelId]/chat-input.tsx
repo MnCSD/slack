f8a30023d7ad5cecb815a32a8aaf48384f64decd
MnCSD <mnikolopoylos@gmail.com>
2024-09-02 01:32:17 +0300

    quill-editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,3 +1,4 @@
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
 import dynamic from "next/dynamic";
 import Quill from "quill";
 import { useRef } from "react";
@@ -10,13 +11,22 @@ interface ChatInputProps {
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const editorRef = useRef<Quill | null>(null);
+  const { data, isPending } = useCreateMessage();
+
+  const handleSubmit = ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {};
 
   return (
     <div className="px-5 w-full">
       <Editor
         variant="create"
         placeholder={placeholder}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         disabled={false}
         innerRef={editorRef}
       />
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,6 +3,7 @@ import React, {
   useEffect,
   useLayoutEffect,
   useRef,
+  useState,
 } from "react";
 import Quill, { QuillOptions } from "quill";
 import { PiTextAa } from "react-icons/pi";
@@ -10,9 +11,12 @@ import { MdSend } from "react-icons/md";
 
 import "quill/dist/quill.snow.css";
 import { Button } from "./ui/button";
-import { ImageIcon, Smile } from "lucide-react";
+import { ImageIcon, Smile, XIcon } from "lucide-react";
 import { Hint } from "./hint";
 import { Delta, Op } from "quill/core";
+import { cn } from "@/lib/utils";
+import { EmojiPopover } from "./emoji-popover";
+import Image from "next/image";
 
 type EditorValue = {
   image: File | null;
@@ -38,12 +42,16 @@ const Editor = ({
   innerRef,
   variant = "create",
 }: EditorProps) => {
+  const [text, setText] = useState("");
+  const [image, setImage] = useState<File | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const submitRef = useRef(onSubmit);
   const placeholderRef = useRef(placeholder);
   const quillRef = useRef<Quill | null>(null);
   const defaultValueRef = useRef(defaultValue);
   const disabledRef = useRef(disabled);
+  const imageRef = useRef<HTMLInputElement>(null);
+  const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
@@ -64,50 +72,154 @@ const Editor = ({
     const options: QuillOptions = {
       theme: "snow",
       placeholder: placeholderRef.current,
+      modules: {
+        toolbar: [
+          ["bold", "italic", "strike"],
+          ["link"],
+          [{ list: "ordered" }, { list: "bullet" }],
+        ],
+        keyboard: {
+          bindings: {
+            enter: {
+              key: "Enter",
+              handler: () => {
+                const text = quill.getText();
+                const addedImage = imageRef.current?.files?.[0] || null;
+
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) return;
+
+                const body = JSON.stringify(quill.getContents());
+
+                submitRef.current?.({ body, image: addedImage });
+              },
+            },
+            shift_enter: {
+              key: "Enter",
+              shiftKey: true,
+              handler: () => {
+                quill.insertText(quill.getSelection()?.index || 0, "\n");
+              },
+            },
+          },
+        },
+      },
     };
 
-    new Quill(editorContainer, options);
+    const quill = new Quill(editorContainer, options);
+    quillRef.current = quill;
+    quillRef.current.focus();
+
+    if (innerRef) {
+      innerRef.current = quill;
+    }
+
+    quill.setContents(defaultValueRef.current);
+    setText(quill.getText());
+
+    quill.on(Quill.events.TEXT_CHANGE, () => {
+      setText(quill.getText());
+    });
 
     return () => {
+      quill.off(Quill.events.TEXT_CHANGE);
+
       if (container) {
         container.innerHTML = "";
       }
+      if (quillRef.current) {
+        quillRef.current = null;
+      }
+      if (innerRef) {
+        innerRef.current = null;
+      }
     };
-  }, []);
+  }, [innerRef]);
+
+  const toggleTollbar = () => {
+    setIsToolbarVisible((current) => !current);
+
+    const tollbarElement = containerRef.current?.querySelector(".ql-toolbar");
+
+    if (tollbarElement) {
+      tollbarElement.classList.toggle("hidden");
+    }
+  };
+
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+
+    quill?.insertText(quill.getSelection()?.index || 0, emoji.native);
+  };
+
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageRef}
+        onChange={(e) => setImage(e.target.files![0])}
+        className="hidden"
+      />
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
+        {!!image && (
+          <div className="p-2">
+            <div className="relative size-[62px] flex items-center justify-center group/image">
+              <Hint label="Remove image" align="center">
+                <button
+                  onClick={() => {
+                    setImage(null);
+                    imageRef.current!.value = "";
+                  }}
+                  className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                >
+                  <XIcon className="size-3.5" />
+                </button>
+              </Hint>
+              <Image
+                src={URL.createObjectURL(image)}
+                alt="Uploaaded"
+                className="rounded-xl overflow-hidden border object-cover"
+                fill
+              />
+            </div>
+          </div>
+        )}
         <div className="flex px-2 pb-2 z-[5]">
-          <Hint label="Hide formatting" align="center">
+          <Hint
+            label={isToolbarVisible ? "Hide formatting" : "Show formatting"}
+            align="center"
+          >
             <Button
-              disabled={false}
+              disabled={disabled}
               size="sm"
-              onClick={() => {}}
+              onClick={toggleTollbar}
               variant={"ghost"}
             >
               <PiTextAa className="size-4 " />
             </Button>
           </Hint>
 
-          <Hint label="Emoji" align="center">
-            <Button
-              disabled={false}
-              size="sm"
-              onClick={() => {}}
-              variant={"ghost"}
-            >
+          <EmojiPopover onEmojiSelect={onEmojiSelect}>
+            <Button disabled={disabled} size="sm" variant={"ghost"}>
               <Smile className="size-4 " />
             </Button>
-          </Hint>
+          </EmojiPopover>
 
           {variant === "create" && (
             <Hint align="center" label="Image">
               <Button
-                disabled={false}
+                disabled={disabled}
                 size="sm"
-                onClick={() => {}}
+                onClick={() => {
+                  imageRef.current?.click();
+                }}
                 variant={"ghost"}
               >
                 <ImageIcon className="size-4 " />
@@ -120,14 +232,19 @@ const Editor = ({
               <Button
                 variant={"outline"}
                 size={"sm"}
-                onClick={() => {}}
+                onClick={onCancel}
                 disabled={false}
               >
                 Cancel
               </Button>
               <Button
-                onClick={() => {}}
-                disabled={false}
+                onClick={() => {
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  });
+                }}
+                disabled={disabled || isEmpty}
                 size={"sm"}
                 className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/20 text-white"
               >
@@ -138,21 +255,38 @@ const Editor = ({
 
           {variant === "create" && (
             <Button
-              disabled={false}
-              onClick={() => {}}
+              disabled={disabled || isEmpty}
+              onClick={() => {
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                });
+              }}
               size={"iconSm"}
-              className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/20 text-white"
+              className={cn(
+                "ml-auto",
+                isEmpty
+                  ? "bg-white hover:bg-white text-muted-foreground"
+                  : "bg-[#007a5a] hover:bg-[#007a5a]/20 text-white"
+              )}
             >
               <MdSend className="size-4 " />
             </Button>
           )}
         </div>
       </div>
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Return</strong> to add a new line
-        </p>
-      </div>
+      {variant === "create" && (
+        <div
+          className={cn(
+            "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
+            !isEmpty && "opacity-100"
+          )}
+        >
+          <p>
+            <strong>Shift + Enter</strong> to add a new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
